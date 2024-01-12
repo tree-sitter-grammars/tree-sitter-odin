10808aa05d1e9be49a4aba0417ea2c301b0f2e91
player01osu <player01osu@gmail.com>
2024-01-12 18:48:45 -0500

    Fix strange indenting in `else` and `else if` blocks

--- queries/indents.scm
+++ queries/indents.scm
@@ -4,11 +4,7 @@
   (overloaded_procedure_declaration)
   (enum_declaration)
   (union_declaration)
-  (if_statement)
-  (else_if_clause)
-  (else_clause)
   (when_statement)
-  (else_when_clause)
   (for_statement)
   (switch_case)
 ] @indent.begin
